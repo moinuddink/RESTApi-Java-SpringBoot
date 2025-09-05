d4b81ca308f77a7730e8b53142ac45048232763d
moinuddin06218 <moinuddin06218@gmail.com>
2025-09-04 22:31:26 -0400

    pushing-the-first-code-REST

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,34 @@
+package com.in28minutes.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	// hello-world
+	
+	@GetMapping( path ="/helloworld")
+	public String helloWorld()
+	{
+		return "Hello World";
+	}
+	
+	@GetMapping( path ="/hello-world-bean")
+	public HelloWorldBean helloWorldBean()
+	{
+		return new HelloWorldBean ("Hello World");
+	}
+	
+	// Path Parameters
+	// /users/{id}/todos/{id} =>/users/2/todos/200
+	// /hello-world/path-variable/{name}
+	
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+
+}
